d4327fb8634eafdf1e4e759eacbf9930edfcbad6
E-Kiani <70318282+E-Kiani@users.noreply.github.com>
2022-02-11 17:37:08 +0330

    Update main.py

--- GUI Base64/GUI B64Decoder/main.py
+++ GUI Base64/GUI B64Decoder/main.py
@@ -3,15 +3,6 @@ from tkinter import *
 from tkinter import ttk
 from tkinter import messagebox
 
-
-Window = Tk()
-Window.title("Base64 Decoder")
-Window.geometry("520x570+300+115")
-Window.resizable(False, False)
-
-Font = "Arial"
-
-
 def encode():
     Outbox.config(state="normal")
     Outbox.delete("1.0", "end-1c")
@@ -19,17 +10,18 @@ def encode():
     # print(f"-{Base64Decode(Inputbox.get('1.0', 'end-1c'))}-")
     Outbox.insert(END, Base64Decode(Inputbox.get("1.0", "end-1c")))
     Outbox.config(state="disabled")
+    
+    
+Window = Tk()
+Window.title("Base64 Decoder")
+Window.geometry("520x570+300+115")
+Window.resizable(False, False)
 
-
+Font = "Arial"
 Title = Label(Window, text="Welcome To My Base64 Decoder", font=(24))
-
 Inputbox = Text(Window, width=50, bd=3, font=(Font, 15))
-
-Do_Btn = ttk.Button(Window, text="Decode",
-                    cursor="hand2", command=encode)
-
+Do_Btn = ttk.Button(Window, text="Decode", cursor="hand2", command=encode)
 Outbox = Text(Window, width=50, bd=3, font=(Font, 15))
-
 CopyRight = Label(Window, text="""
 ©2022 Esfandiar Kiani, All rights reserved.
 """)
