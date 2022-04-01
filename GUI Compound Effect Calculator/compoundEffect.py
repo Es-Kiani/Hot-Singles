68cf67809ada7e0c14c96280169dd51d54758d5f
E-Kiani <70318282+E-Kiani@users.noreply.github.com>
2022-04-01 18:51:46 +0430

    Add files via upload

--- GUI Compound Effect Calculator/compoundEffect.py
+++ GUI Compound Effect Calculator/compoundEffect.py
@@ -1,11 +1,21 @@
-def CompoundEffect(got_Asset, got_Percent, got_Step) -> float:
-    
+import datetime
+
+
+def CompoundEffect(got_Asset, got_Percent, got_Step) -> float:    
     
     asset, percent, step = abs(got_Asset), abs(got_Percent), abs(got_Step)
-    
-    for i in range(step):
-        p = percent * (asset / 100)
-        asset += p
+    now_Time = datetime.datetime.now()
+    with open("C:/Users/Masuod/Desktop/Result.txt", "a+") as file:
+        file.write(f"{now_Time}: \n")
+        file.write(f"asset:{got_Asset} percent:{got_Percent} step:{got_Step}  \n")
+        
+        for i in range(1, step + 1):
+            pAsset = asset
+            p = percent * (asset / 100)
+            asset += p
+            file.write(f"Step{i}: {round(pAsset, 1)}\t+\t{got_Percent}%({round(p, 1)})\t=\t{round(asset, 1)}  \n")
+
+        file.write(f"\n\n")
     
     return round(asset, 1)
     
