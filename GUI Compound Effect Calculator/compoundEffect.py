69e7b1d620bff32c0ce32762bca261ce26ad6350
E-Kiani <70318282+E-Kiani@users.noreply.github.com>
2022-04-06 01:23:39 +0430

    Add files via upload
    V.1.3

--- GUI Compound Effect Calculator/compoundEffect.py
+++ GUI Compound Effect Calculator/compoundEffect.py
@@ -1,22 +1,27 @@
 import datetime
-import sys
+import os
+
+def writeToFile(data:str, path=os.getcwd(), fName="Result.txt", fOType="a+"):
+    with open(f"{path}/{fName}", fOType) as file:
+        file.write(str(data))
+    return
+    
 
 
 def CompoundEffect(got_Asset, got_Percent, got_Step) -> float:
     now_Time = datetime.datetime.now()
     asset, percent, step = abs(got_Asset), abs(got_Percent), abs(got_Step)
     
-    with open(f"{os.getcwd()}/Result.txt", "a+") as file:
-        file.write(f"{now_Time}: \n")
-        file.write(f"asset:{got_Asset} percent:{got_Percent} step:{got_Step}  \n")
-        
-        for i in range(1, step + 1):
-            pAsset = asset
-            p = percent * (asset / 100)
-            asset += p
-            file.write(f"Step{i}: {round(pAsset, 1)}\t+\t{got_Percent}%({round(p, 1)})\t=\t{round(asset, 1)}  \n")
+    writeToFile(f"{now_Time}: \n")
+    writeToFile(f"asset:{got_Asset} percent:{got_Percent} step:{got_Step}  \n")
+    
+    for i in range(1, step + 1):
+        pAsset = asset
+        p = percent * (asset / 100)
+        asset += p
+        writeToFile(f"Step{i}: {round(pAsset, 1)}\t+\t{got_Percent}%({round(p, 1)})\t=\t{round(asset, 1)}  \n")
 
-        file.write(f"\n\n")
+    writeToFile(f"\n\n")
     
     return round(asset, 1)
     
