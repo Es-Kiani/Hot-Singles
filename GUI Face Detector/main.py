b82d1a3e21f12616fe59a8517a6aaaf98cfe554e
Stephen <masuod333@gmail.com>
2024-03-10 03:26:56 +0330

    Delete GUI Face Detector directory

--- GUI Face Detector/main.py
+++
@@ -1,9 +0,0 @@
-from Module.GUIFaceDetector import GUIFaceDetector as faceDetector
-
-
-webcam_URL = "https://192.168.1.13:8569/video"
-xml = "haarcascade_frontalface_default.xml"
-
-
-if __name__ == "__main__":
-    faceDetector().run()
